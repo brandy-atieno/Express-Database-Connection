92ef9dc618996880a0e4c95b607e9076e364b654
Brandy CAYPE <brandycype234@gmail.com>
2022-07-04 09:42:01 +0300

    Express API Database Connection

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-const express = require('express')
+const express = require("express")
 const bodyparser = require('body-parser');
+const {router}=require('./routes/users')
 const port=5000
 const server=express()
 server.use(bodyparser.json());
 server.use(bodyparser.urlencoded({extended: true}));
-const {router}=require('./routes/users')
+
 
 server.use('/', router)
 server.use((req,res,next)=>{
